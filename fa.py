d7c8eb20d4f2616a0f68abe58f960b08a05c26c7
Robbie Clarken <rob.clarken@gmail.com>
2013-03-04 17:20:51 +1100

    Added documentation and removed unwanted parameters

--- fa.py
+++ fa.py
@@ -37,53 +37,81 @@ def pv_list_to_str(pv_list):
         previous_pv = this_pv
     return pv_str
 
-def capture(pv_list, samples=1, server=None, port=None, start_date=None,
-            continuous=True, data_format=FULL_RATE, data_mask=None,
-            all_data=False, raw_format=False, forbid_gaps=False,
-            check_gaps=False, extra_dimensions=False, data_name=None,
-            utc=False, subtract_day=False, controller_info=False):
+def capture(pv_list, samples=None, server=None, port=None, start_date=None,
+            end_date=None, continuous=True, data_format=None, data_mask=None,
+            extra_dimensions=False, utc=False, subtract_day=False,
+            controller_info=False):
+    """
+    pv_list: list or string
+        A list of ids or a string in formatted such as "1-9,30-35,41"
+    samples: int or timedelta
+        Amount of data to collect in number of samples to read or a
+        length of time. Not required if start_date and end_date are
+        specified.
+    server: str, optional
+        Address of server running fa-archiver.
+    port: int, optional
+        Port of server running fa-archiver.
+    start_date: datetime, optional
+        Earliest point of historical data to fetch.
+        Not required for continuous capture.
+    end_date: datetime
+        Latest point of historical data to fetch.
+        Not required for continuous capture or if samples is specified.
+    continuous: bool
+        Capture live data.
+    data_format: one of {FULL_RATE, SINGLE_DECIMATED, DOUBLE_DECIMATED}, optional
+    data_mask: one of {MEAN_MASK, MIN_MASK, MAX_MASK, STD_DEV_MASK}, optional
+    extra_dimension: bool
+        Keep extra dimensions.
+    utc: bool
+        Use UTC timestamps instead of local time.
+    subtract_day: bool
+        Subtract the day of the first timestamp from the timestamp vector.
+    controller_info: bool
+        Save the id0 communication controller timestamp information.
+    """
 
     options = ['-q']
     if server:
         options += ['-S', server]
     if port:
         options += ['-p', port]
+    s_option = None
     if start_date:
-        options += ['-s', start_date.isoformat()]
         continuous = False
+        s_option = start_date.isoformat()
+    if end_date:
+        s_option += '~%s' % start_date.isoformat()
+    elif not samples:
+        raise(Error('samples or end_data must be specified'))
+    if s_option:
+        options += ['-a', '-s', s_option]
     if continuous:
         options += ['-C']
+    if data_mask and not data_format:
+        data_format = FULL_RATE
     if data_format:
         if data_mask:
             data_format += '%i' % data_mask
         options += ['-f', data_format]
-    if all_data:
-        options += ['-a']
-    if raw_format:
-        options += ['-R']
-    if forbid_gaps:
-        options += ['-c']
-    if check_gaps:
-        options += ['-z']
     if extra_dimensions:
         options += ['-k']
-    if data_name:
-        options += ['-n', data_name]
     if utc:
         options += ['-Z']
     if subtract_day:
         options += ['-d']
     if controller_info:
-        options += ['T']
+        options += ['-T']
 
     command = ['fa-capture'] + options + [pv_list_to_str(pv_list)]
     if samples:
-        command += [ '%i' % samples ]
+        if hasattr(samples, 'total_seconds'):
+            command += ['%is' % samples.total_seconds()]
+        else:
+            command += ['%i' % samples]
     capture_process = subprocess.Popen(command, stdout=subprocess.PIPE)
     stdout = capture_process.communicate()[0]
-
-    if raw_format:
-        return stdout
 
     with NamedTemporaryFile('wr+b', delete=False) as mat_file:
         mat_file.write(stdout)
